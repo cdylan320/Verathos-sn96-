a4f5a271e33b969f4bacf6b8593b82e3b0b19fc9
Keplerteron <134685552+Keplerteron@users.noreply.github.com>
2026-08-09 06:03:19 +0200

    chore: bump fp8 compatibility versions

--- neurons/version.py
+++ neurons/version.py
@@ -78,7 +78,7 @@ def _decode(version: int) -> tuple[int, int, int]:
 
 MINER_MAJOR = 0
 MINER_MINOR = 1
-MINER_PATCH = 4
+MINER_PATCH = 5
 
 miner_version: int = _encode(MINER_MAJOR, MINER_MINOR, MINER_PATCH)
 miner_version_str: str = _version_str(MINER_MAJOR, MINER_MINOR, MINER_PATCH)
@@ -92,7 +92,7 @@ miner_version_str: str = _version_str(MINER_MAJOR, MINER_MINOR, MINER_PATCH)
 
 VALIDATOR_MAJOR = 0
 VALIDATOR_MINOR = 1
-VALIDATOR_PATCH = 29
+VALIDATOR_PATCH = 30
 
 validator_version: int = _encode(VALIDATOR_MAJOR, VALIDATOR_MINOR, VALIDATOR_PATCH)
 validator_version_str: str = _version_str(VALIDATOR_MAJOR, VALIDATOR_MINOR, VALIDATOR_PATCH)
